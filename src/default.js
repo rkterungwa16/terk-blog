3b340be35bce6656c6dd5e2034729ef2326d5f3d
rkterungwa16 <terungwakombol@gmail.com>
2020-07-01 22:43:42 +0200

    add editor config home page styles

--- src/default.js
+++ src/default.js
@@ -1,7 +1,7 @@
 const { readFile } = require("./file/read");
 
 module.exports = async function (req, res) {
-    const html = await readFile(`${__dirname}/template/page.html`);
+    const html = await readFile(`${__dirname}/template/home.html`);
     res.writeHead(200, {
         "Content-Type": "text/html",
     });
